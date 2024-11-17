87739e5cda48255c9109952ddc933f6178426297
Pinminh <minh.nguyenhongk22@hcmut.edu.vn>
2024-11-18 05:04:41 +0700

    Hong Minh committed - finish 1.2, 2.2.1

--- backend/report/views.py
+++ backend/report/views.py
@@ -1,17 +1,42 @@
+import json
 from .serializers import ReportSerializer
 from .models import Report
-from rest_framework import viewsets
+from user.models import User
+from rest_framework import viewsets, mixins
+from rest_framework.permissions import IsAdminUser
 from rest_framework.response import Response
+from rest_framework.exceptions import ValidationError
+from rest_framework.status import HTTP_204_NO_CONTENT
+from django.shortcuts import get_object_or_404, get_list_or_404
 
-
-# Create your views here.
-class ReportViewSet(viewsets.ModelViewSet):
+class ReportViewSet(
+    viewsets.GenericViewSet,
+    mixins.CreateModelMixin,
+):
     queryset = Report.objects.all()
     serializer_class = ReportSerializer
-
-
-class UserReportViewSet(viewsets.ViewSet):
-    def retrieve(self, request, pk=None):
-        reports = Report.objects.filter(user=pk)
-        serializer = ReportSerializer(reports, many=True)
+    
+    
+    def get(self, request, *args, **kwargs):
+        try:
+            body = json.loads(request.body)
+            user_id = body.get('user_id')
+            report_id = body.get('report_id')
+        except json.JSONDecodeError:
+            user_id = report_id = None
+        
+        if user_id and report_id:
+            raise ValidationError(detail='Ambiguity when supplying both user_id and report_id.')
+        
+        if report_id:
+            report = get_object_or_404(Report, id=report_id)
+            serializer = self.serializer_class(report)
+            return Response(serializer.data)
+        
+        if user_id:
+            reports = get_object_or_404(User, user_id=user_id).report_set.all()
+        else:
+            reports = get_list_or_404(Report)
+        
+        serializer = self.serializer_class(reports, many=True)
         return Response(serializer.data)
