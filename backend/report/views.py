b0020f00711107815524b6aebcfceb4951d58ab9
Pinminh <minh.nguyenhongk22@hcmut.edu.vn>
2024-11-18 05:06:20 +0700

    Hong Minh committed - finish 4.2.1

--- backend/report/views.py
+++ backend/report/views.py
@@ -40,3 +40,24 @@ class ReportViewSet(
         
         serializer = self.serializer_class(reports, many=True)
         return Response(serializer.data)
+    
+    
+    def get_permissions(self):
+        if self.action == 'destroy':
+            return [IsAdminUser]
+        return super().get_permissions()
+    
+    
+    def delete(self, request, *args, **kwargs):
+        try:
+            body = json.loads(request.body)
+            report_id = body.get('report_id')
+        except json.JSONDecodeError:
+            report_id = None
+        
+        if not report_id:
+            raise ValidationError(detail='No report_id provided.')
+        
+        report = get_object_or_404(Report, id=report_id)
+        report.delete()
+        return Response(status=HTTP_204_NO_CONTENT)
