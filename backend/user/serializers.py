f332f1548184602ffebfea1026ad9dbcd5d48279
Huynh Thanh Tam <tam.huynhk22cs@gmail.com>
2024-11-23 10:14:06 +0700

    Thanh Tam committed finish 1.1.123 3.1.1 4.1.1

--- backend/user/serializers.py
+++ backend/user/serializers.py
@@ -11,15 +11,3 @@ class UserProfileSerializer(serializers.ModelSerializer):
         user = User.objects.create_user(**validated_data)
         return user
 
-class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields ='__all__'
-    def __str__(self):
-       return self.name
-
-    extra_kwargs = {'password':{
-        'write_only': True,
-        'required': True,
-    }}
-
