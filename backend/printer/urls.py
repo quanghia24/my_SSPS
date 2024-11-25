777b423c3dd792c34972605b34ae01a8ea45e920
Lê Nguyễn Gia Hưng <ghung21112004@gmail.com>
2024-11-25 09:38:09 +0700

    add path add_filetypes in urls

--- backend/printer/urls.py
+++ backend/printer/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('<int:pk>/', views.printer_details, name = 'printer-details'),
     path('active/', views.active_printers, name = 'active_printers'),
     path('brand/<str:brand>/', views.brand_printers, name = 'brand_printers'),
+    path('add_filetypes/', views.add_filetypes, name='add_filetypes'),
 ]
