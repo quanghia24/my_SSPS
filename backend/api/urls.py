0aeb3129fe5d19473ef2dbdfd2151d1493150bf8
Lê Nguyễn Gia Hưng <ghung21112004@gmail.com>
2024-11-25 11:13:58 +0700

    Gia Hung commited - finish 2.3.1, 2.3.2, 2.3.3, 2.3.4

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from django.urls import path, include
 
 
@@ -11,18 +10,3 @@ urlpatterns = [
     path('reports/', include('report.urls')),
     
 ]
- 
-=======
-from django.urls import path, include
-
-
-urlpatterns = [
-
-    path('users/', include('user.urls')),
-    path('buys/', include('buy.urls')),
-    path('prints/', include('print.urls')),
-    path('printers/', include('printer.urls')),
-    path('reports/', include('report.urls')),
-    
-]
->>>>>>> 7224af874d5f599fdc2bf7844775cfec69806f47
