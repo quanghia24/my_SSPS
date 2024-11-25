af314df1f2757003a60df3a1984f705400cc916d
quanghia24 <nghiaqcqs@gmail.com>
2024-11-25 21:54:37 +0700

    Quang Nghia committed - remove sensitive data, test connection with cloud database

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -1,5 +1,9 @@
 from pathlib import Path
 import os
+import environ
+env = environ.Env()
+environ.Env.read_env()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 from datetime import timedelta
@@ -149,11 +153,11 @@ WSGI_APPLICATION = 'core.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql', 
-        'NAME': 'defaultdb',
-        'USER': 'avnadmin',
-        'PASSWORD': 'AVNS_-ChbzYZjql8pUKDd33Z',
-        'HOST': 'ssps-quanghia-quanghia.b.aivencloud.com',   # Or an IP Address that your DB is hosted on
-        'PORT': '25554',
+        'NAME': env('DB_NAME'),
+        'USER': env('DB_USER'),
+        'PASSWORD': env('DB_PASSWORD'),
+        'HOST': env('DB_HOST'),
+        'PORT': env('DB_PORT')
     }
 }
 
